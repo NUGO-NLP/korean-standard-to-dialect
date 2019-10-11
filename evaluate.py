4db3b2ad8d1dea181de510cabae1fb88814e86fe
sjy366 <sjy20131565@gmail.com>
2019-10-11 11:50:05 +0900

    OOV 정확도 및 표준어 그대로 출력했을 때 정확도 구하는 코드추가

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,19 @@
+def word_accuracy_oov(sentence_list, word_dict):
+    size = 0
+    count = 0
+    for std, dia, inf in sentence_list:
+        word_dict_std = std.split()
+        word_list_dia = dia.split()
+        word_list_inf = inf.split()
+        for i in range(len(word_list_dia)):
+            if word_dict_std[i] not in word_dict:
+                #print(word_dict_std[i], word_list_dia[i], word_list_inf[i])
+                size = size + 1
+                if word_list_dia[i] == word_list_inf[i]:
+                    count = count + 1
+
+    return count / size
+
 def word_accuracy(sentence_list):
     size = 0
     count = 0
--- main.py
+++ main.py
@@ -2,33 +2,41 @@ import json
 import os
 
 from model.rule_base import ruleBase
-from evaluate import word_accuracy, sentence_accuracy
+from evaluate import word_accuracy, sentence_accuracy, word_accuracy_oov
 
 current_path = os.path.dirname(os.path.abspath( __file__ ))
-sent_gs_valid_filename = os.path.join(current_path, 'data/sent_gs_valid.json')
-sent_jl_valid_filename = os.path.join(current_path, 'data/sent_jl_valid.json')
+sent_gs_valid_filename = os.path.join(current_path, 'data/sent_gs_test.json')
+sent_jl_valid_filename = os.path.join(current_path, 'data/sent_jl_test.json')
 result_gs_filename = os.path.join(current_path, 'output/sent_result_gs.json')
 result_jl_filename = os.path.join(current_path, 'output/sent_result_jl.json')
+word_dict_gs_filename = os.path.join(current_path, 'model/save/rulebase_word_dict_gs.json')
+word_dict_jl_filename = os.path.join(current_path, 'model/save/rulebase_word_dict_jl.json')
 
 rule_base_model_gs = ruleBase('gs')
 rule_base_model_jl = ruleBase('jl')
 
 def test_and_write(model, input_filename, output_filename):
-    sentence_valid = dict()
+    sent_test = dict()
     with open(input_filename) as input_file:
-        sentence_valid = json.load(input_file)
+        sent_test = json.load(input_file)
     with open(output_filename, 'w', encoding='UTF-8') as output_file:
-        for sentence in sentence_valid:
+        for sentence in sent_test:
             standard = sentence['standard']
             inference = model.inference_sentence(standard)
             sentence['inference'] = inference
-        output_file.write(json.dumps(sentence_valid, ensure_ascii=False, indent='\t'))
-    return sentence_valid
+        output_file.write(json.dumps(sent_test, ensure_ascii=False, indent='\t'))
+    return sent_test
 
 def make_sentence_list(sent_dict):
     ret_list = list()
     for sent in sent_dict:
         ret_list.append((sent['standard'], sent['dialect'], sent['inference']))
+    return ret_list
+
+def make_sentence_list_same(sent_dict):
+    ret_list = list()
+    for sent in sent_dict:
+        ret_list.append((sent['standard'], sent['dialect'], sent['standard']))
     return ret_list
 
 if __name__ == "__main__":
@@ -37,8 +45,31 @@ if __name__ == "__main__":
     sent_list = make_sentence_list(sent_dict)
     print('[gs] word_accuracy:', word_accuracy(sent_list))
     print('[gs] sentence_accuracy:', sentence_accuracy(sent_list))
+    with open(word_dict_gs_filename) as word_dict_gs_file:
+        word_dict_gs = json.load(word_dict_gs_file)
+        print('[gs] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_gs))
+    
+    print()
+    sent_list_same = make_sentence_list_same(sent_dict)
+    print('[gs_same] word_accuracy:', word_accuracy(sent_list_same))
+    print('[gs_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
+    with open(word_dict_gs_filename) as word_dict_gs_file:
+        word_dict_gs = json.load(word_dict_gs_file)
+        print('[gs_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_gs))
 
+    print()
     sent_dict = test_and_write(rule_base_model_jl, sent_jl_valid_filename, result_jl_filename)
     sent_list = make_sentence_list(sent_dict)
     print('[jl] word_accuracy:', word_accuracy(sent_list))
     print('[jl] sentence_accuracy:', sentence_accuracy(sent_list))
+    with open(word_dict_jl_filename) as word_dict_jl_file:
+        word_dict_jl = json.load(word_dict_jl_file)
+        print('[jl] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_jl))
+    
+    print()
+    sent_list_same = make_sentence_list_same(sent_dict)
+    print('[jl_same] word_accuracy:', word_accuracy(sent_list_same))
+    print('[jl_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
+    with open(word_dict_jl_filename) as word_dict_jl_file:
+        word_dict_jl = json.load(word_dict_jl_file)
+        print('[jl_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_jl))
