7dd22f275a2bbfaeb2d47eb11c8b76d499b3f771
Jungyeon Sohn <sjy20131565@gmail.com>
2019-10-16 01:20:01 +0900

    기존 OOV 일때 subword단위로 추론하도록 개선

--- main.py
+++ main.py
@@ -16,15 +16,20 @@ statistical_model_gs = statisticalModel('gs')
 statistical_model_jl = statisticalModel('jl')
 
 def test_and_write(model, input_filename, output_filename):
+    print('\ntest_and_write %s start!' % input_filename)
     sent_test = dict()
     with open(input_filename) as input_file:
         sent_test = json.load(input_file)
     with open(output_filename, 'w', encoding='UTF-8') as output_file:
+        count = 0
         for sentence in sent_test:
+            print('\r%d/%d' % (count, len(sent_test)), end='')
             standard = sentence['standard']
             inference = model.inference_sentence(standard)
             sentence['inference'] = inference
+            count = count + 1
         output_file.write(json.dumps(sent_test, ensure_ascii=False, indent='\t'))
+    print('\ntest_and_write %s finished!' % input_filename)
     return sent_test
 
 def make_sentence_list(sent_dict):
@@ -41,35 +46,39 @@ def make_sentence_list_same(sent_dict):
 
 if __name__ == "__main__":
     
-    sent_dict = test_and_write(statistical_model_gs, sent_gs_valid_filename, result_gs_filename)
-    sent_list = make_sentence_list(sent_dict)
-    print('[gs] word_accuracy:', word_accuracy(sent_list))
-    print('[gs] sentence_accuracy:', sentence_accuracy(sent_list))
-    with open(word_dict_gs_filename) as word_dict_gs_file:
-        word_dict_gs = json.load(word_dict_gs_file)
-        print('[gs] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_gs))
+    # sent_dict = test_and_write(statistical_model_gs, sent_gs_valid_filename, result_gs_filename)
+    # sent_list = make_sentence_list(sent_dict)
+    # print('[gs] word_accuracy:', word_accuracy(sent_list))
+    # print('[gs] sentence_accuracy:', sentence_accuracy(sent_list))
+    # with open(word_dict_gs_filename) as word_dict_gs_file:
+    #     word_dict_gs = json.load(word_dict_gs_file)
+    #     print('[gs] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_gs))
     
-    print()
-    sent_list_same = make_sentence_list_same(sent_dict)
-    print('[gs_same] word_accuracy:', word_accuracy(sent_list_same))
-    print('[gs_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
-    with open(word_dict_gs_filename) as word_dict_gs_file:
-        word_dict_gs = json.load(word_dict_gs_file)
-        print('[gs_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_gs))
+    # print()
+    # sent_list_same = make_sentence_list_same(sent_dict)
+    # print('[gs_same] word_accuracy:', word_accuracy(sent_list_same))
+    # print('[gs_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
+    # with open(word_dict_gs_filename) as word_dict_gs_file:
+    #     word_dict_gs = json.load(word_dict_gs_file)
+    #     print('[gs_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_gs))
 
-    print()
-    sent_dict = test_and_write(statistical_model_jl, sent_jl_valid_filename, result_jl_filename)
-    sent_list = make_sentence_list(sent_dict)
-    print('[jl] word_accuracy:', word_accuracy(sent_list))
-    print('[jl] sentence_accuracy:', sentence_accuracy(sent_list))
-    with open(word_dict_jl_filename) as word_dict_jl_file:
-        word_dict_jl = json.load(word_dict_jl_file)
-        print('[jl] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_jl))
+    # print()
+    # sent_dict = test_and_write(statistical_model_jl, sent_jl_valid_filename, result_jl_filename)
+    # sent_list = make_sentence_list(sent_dict)
+    # print('[jl] word_accuracy:', word_accuracy(sent_list))
+    # print('[jl] sentence_accuracy:', sentence_accuracy(sent_list))
+    # with open(word_dict_jl_filename) as word_dict_jl_file:
+    #     word_dict_jl = json.load(word_dict_jl_file)
+    #     print('[jl] word_accuracy_oov:', word_accuracy_oov(sent_list, word_dict_jl))
     
-    print()
-    sent_list_same = make_sentence_list_same(sent_dict)
-    print('[jl_same] word_accuracy:', word_accuracy(sent_list_same))
-    print('[jl_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
-    with open(word_dict_jl_filename) as word_dict_jl_file:
-        word_dict_jl = json.load(word_dict_jl_file)
-        print('[jl_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_jl))
+    # print()
+    # sent_list_same = make_sentence_list_same(sent_dict)
+    # print('[jl_same] word_accuracy:', word_accuracy(sent_list_same))
+    # print('[jl_same] sentence_accuracy:', sentence_accuracy(sent_list_same))
+    # with open(word_dict_jl_filename) as word_dict_jl_file:
+    #     word_dict_jl = json.load(word_dict_jl_file)
+    #     print('[jl_same] word_accuracy_oov:', word_accuracy_oov(sent_list_same, word_dict_jl))
+
+    while True:
+        q = input()
+        print(statistical_model_jl.inference_sentence(q))
--- model/statistical.py
+++ model/statistical.py
@@ -1,5 +1,6 @@
 import os
 import json
+from konlpy.tag import Kkma
 
 current_path = os.path.dirname(os.path.abspath( __file__ ))
 parent_path = os.path.join(current_path, '..')
@@ -9,17 +10,27 @@ class statisticalModel:
         # Region can only be 'gs' or 'jl'
         assert region == 'gs' or region == 'jl', 'region should be \'gs\' or \'jl\''
 
+        self.kkma = Kkma()
         self.region = region
         self.sent_dict = dict()
         self.word_dict = dict()
-
+        self.sent_dict_subword = dict()
+        self.word_dict_subword = dict()
+        
         self.sentence_data_filename = os.path.join(parent_path, 'data/sent_' + region + '_train.json')
         self.word_data_filename = os.path.join(parent_path, 'data/word_' + region + '_train.json')
+        self.sentence_data_ex_filename = os.path.join(parent_path, 'data/ex/sent_' + region + '_train.json')
+        self.word_data_ex_filename = os.path.join(parent_path, 'data/ex/word_' + region + '_train.json')
+
         self.sentence_dict_filename = os.path.join(current_path, 'save/statistical_sent_dict_' + region + '.json')
         self.word_dict_filename = os.path.join(current_path, 'save/statistical_word_dict_' + region + '.json')
+        self.sentence_dict_ex_filename = os.path.join(current_path, 'save/ex/statistical_sent_dict_' + region + '.json')
+        self.word_dict_ex_filename = os.path.join(current_path, 'save/ex/statistical_word_dict_' + region + '.json')
+        
 
         # If there is a dictionary created
-        if os.path.isfile(self.sentence_dict_filename) and os.path.isfile(self.word_dict_filename):
+        if os.path.isfile(self.sentence_dict_filename) and os.path.isfile(self.word_dict_filename) and \
+            os.path.isfile(self.sentence_dict_ex_filename) and os.path.isfile(self.word_dict_ex_filename):
             self.load_dict()
             print('Load dictionary for %s' % self.region)
         else:
@@ -36,6 +47,16 @@ class statisticalModel:
                 new_sentence['dialect_word_list'] = sentence['dialect'].split()
 
                 self.sent_dict[str(sentence['id'])] = new_sentence
+        
+        with open(self.sentence_data_ex_filename) as sentence_data_ex_file:
+            sentence_list = json.load(sentence_data_ex_file)
+
+            for sentence in sentence_list:
+                new_sentence = dict()
+                new_sentence['standard_word_list'] = sentence['standard'].split()
+                new_sentence['dialect_word_list'] = sentence['dialect'].split()
+
+                self.sent_dict_subword[str(sentence['id'])] = new_sentence
 
         with open(self.word_data_filename) as word_data_file:
             word_list = json.load(word_data_file)
@@ -55,12 +76,40 @@ class statisticalModel:
                     self.word_dict[standard][dialect] = list()
 
                 self.word_dict[standard][dialect].append(str(sentence_id))
+        
+        with open(self.word_data_ex_filename) as word_data_ex_file:
+            word_list = json.load(word_data_ex_file)
+
+            for word in word_list:
+                sentence_id = word['sentence']
+                standard = word['standard']
+                dialect = word['dialect']
+                
+                if standard == dialect:
+                    continue
+
+                if len(standard) == 1:
+                    continue
+
+                if standard not in self.word_dict_subword:
+                    self.word_dict_subword[standard] = dict()
+
+                if dialect not in self.word_dict_subword[standard]:
+                    self.word_dict_subword[standard][dialect] = list()
+
+                self.word_dict_subword[standard][dialect].append(str(sentence_id))
 
         with open(self.sentence_dict_filename, 'w', encoding='UTF-8') as sentence_dict_file:
             sentence_dict_file.write(json.dumps(self.sent_dict, ensure_ascii=False, indent='\t'))
 
+        with open(self.sentence_dict_ex_filename, 'w', encoding='UTF-8') as sentence_dict_ex_file:
+            sentence_dict_ex_file.write(json.dumps(self.sent_dict_subword, ensure_ascii=False, indent='\t'))
+
         with open(self.word_dict_filename, 'w', encoding='UTF-8') as word_dict_file:
             word_dict_file.write(json.dumps(self.word_dict, ensure_ascii=False, indent='\t'))
+
+        with open(self.word_dict_ex_filename, 'w', encoding='UTF-8') as word_dict_ex_file:
+            word_dict_ex_file.write(json.dumps(self.word_dict_subword, ensure_ascii=False, indent='\t'))
 
     def load_dict(self):
         with open(self.sentence_dict_filename) as sentence_dict_file:
@@ -68,6 +117,12 @@ class statisticalModel:
 
         with open(self.word_dict_filename) as word_dict_file:
             self.word_dict = json.load(word_dict_file)
+        
+        with open(self.sentence_dict_ex_filename) as sentence_dict_ex_file:
+            self.sent_dict_subword = json.load(sentence_dict_ex_file)
+
+        with open(self.word_dict_ex_filename) as word_dict_ex_file:
+            self.word_dict_subword = json.load(word_dict_ex_file)
 
     def get_score(self, sentence, sentence_id_list):
         score = 0
@@ -81,6 +136,49 @@ class statisticalModel:
         
         return score / sentence_list_size
 
+    def inference_subword(self, word, sentence):
+        max_sentence_list_len = 0
+        word_infer = ''
+        if word in self.word_dict_subword:
+            dialect_word_dict = self.word_dict_subword[word]
+            for dialect_word in dialect_word_dict.keys():
+                sentence_id_list = dialect_word_dict[dialect_word]
+                if len(sentence_id_list) > max_sentence_list_len:
+                    max_sentence_list_len = len(sentence_id_list)
+                    word_infer = dialect_word
+            return word_infer
+        else:
+            return word
+
+    def inference_word_by_subword(self, target, sentence):
+        # Make sentences in subword units.
+        word_list = sentence.split()
+        noun_list = self.kkma.nouns(sentence)
+
+        sentence_subword = ''
+        for word in word_list:
+            for noun in noun_list:
+                len_noun = len(noun)
+                idx = word.find(noun)
+                if idx != -1:
+                    word = word[:idx] + ' ' + word[idx:idx+len_noun] + ' ' + word[idx+len_noun:]
+                    word = word.strip()
+            sentence_subword = sentence_subword + ' ' + word
+        
+        for noun in noun_list:
+            len_noun = len(noun)
+            idx = target.find(noun)
+            if idx != -1:
+                target = target[:idx] + ' ' + target[idx:idx+len_noun] + ' ' + target[idx+len_noun:]
+                target = target.strip()
+
+        word_infer = ''
+        subword_list = target.split()
+        for subword in subword_list:
+            subword_infer = self.inference_subword(subword, sentence_subword)
+            word_infer = word_infer + subword_infer
+        return word_infer
+
     def inference_word(self, word, sentence='None'):
         if sentence == 'None':
             # Probably not used code.
@@ -90,7 +188,8 @@ class statisticalModel:
                 dialect_word_dict = self.word_dict[word]
                 for dialect_word in dialect_word_dict.keys():
                     sentence_id_list = dialect_word_dict[dialect_word]
-                    if len(sentence_id_list) > max_sentence_list_len:
+                    if len(sentence_id_list) >= max_sentence_list_len:
+                        max_sentence_list_len = len(sentence_id_list)
                         word_infer = dialect_word
                 return word_infer
             else:
@@ -108,7 +207,8 @@ class statisticalModel:
                         max_score = score
                         word_infer = dialect_word
                     elif score == max_score:
-                        if len(sentence_id_list) > max_sentence_list_len:
+                        if len(sentence_id_list) >= max_sentence_list_len:
+                            max_sentence_list_len = len(sentence_id_list)
                             word_infer = dialect_word
                 
                 return word_infer
@@ -116,7 +216,7 @@ class statisticalModel:
                 # If there are no matching words in the dictionary,
                 # just returns a word.
                 # You can use some neural network model for this part.
-                return word
+                return self.inference_word_by_subword(word, sentence)
 
     def inference_sentence(self, sentence):
         word_infer_list = list()
